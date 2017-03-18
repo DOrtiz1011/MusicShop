44ddfd945268c622d6aa8c0dcfe7dbb4550726d1
Daniel Ortiz <DanielOrtiz@JFK.AirTiger.com>
2017-03-18 00:04:15 -0400

    functional modal window

--- MusicShop/App_Start/RouteConfig.cs
+++ MusicShop/App_Start/RouteConfig.cs
@@ -10,10 +10,14 @@ namespace MusicShop
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Album",
+                url: "Home/Album/{id}",
+                defaults: new { controller = "Home", action = "Album", id = UrlParameter.Optional });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
         }
     }
 }
